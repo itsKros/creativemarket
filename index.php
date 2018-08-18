cbd828c114f6e9bf4187d5cc01e2c7f778016124
itsKros <kroshan197@gmail.com>
2018-08-18 15:12:10 +0530

    Latest Rentals & Creative Market
    Latest Rentals & Creative Market Completed with How it works Partials

--- index.php
+++ index.php
@@ -49,15 +49,56 @@
 
     <!-- How it works Section -->
     <section id="howItSec" class="inGrid">
-      <div class="container">
-        <div class="row"></div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>HOW IT WORKS</h2>
+            <img class="img-fluid" src="assets/imgs/howitwork.png">
+          </div>
+        </div>
+      </div>
+      <div class="container hoiw">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="steps">
+              <span>1</span>
+            </div>
+            <p>Find what you want and <br>
+            add it to your cart</p>
+          </div>
+          <div class="col-md-4">
+            <div class="steps">
+              <span>2</span>
+            </div>
+            <p>Payment and inscurance<br>
+            secured via Creative Market</p>
+          </div>
+          <div class="col-md-4">
+            <div class="steps">
+              <span>3</span>
+            </div>
+            <p>Pickup the gear and <br>
+            start creating!</p>
+          </div>
+        </div>
       </div>
     </section>
 
     <!-- CM Stories Section -->
     <section id="cmStories" class="inGrid">
       <div class="container">
-        <div class="row"></div>
+        <div class="row">
+          <div class="offset-md-6 col-md-6">
+            <h2>Creative Market Stories</h2>
+            <p>Stories from real people in our community and how they are using  Creative Market to amplify their work and make the most of their gear.</p>
+            <ul>
+              <li>Cras convallis tellus et elit aliquet 20%</li>
+              <li>Suspendisse tincidunt vulputate leo in sollicitudin</li>
+              <li>Morbi sodales risus quis orci hendrerit semper</li>
+            </ul>
+            <a class="btn btn-primary" href="#">Learn More</a>
+          </div>
+        </div>
       </div>
     </section>
 
@@ -71,7 +112,9 @@
 
               <div class="rentitem">
                 <div class="rentimg">
-                  <span>2 hrs</span>
+                  <div class="renthrs">
+                    <span>2 hrs</span>
+                  </div>
                   <img src="assets/imgs/rent-1.png" alt="">
                 </div>
                 <p class="title">URSA mini 4.6k Pro + Ac charger</p>
@@ -82,7 +125,7 @@
                 <div class="rentimg">
                   <img src="assets/imgs/rent-2.png" alt="">
                 </div>
-                <p class="title">URSA mini 4.6k Pro + Ac charger</p>
+                <p class="title">Sony A7SIII + Accessories</p>
                 <p class="location">Reykjavik, Iceland</p>
               </div>
               
@@ -90,7 +133,7 @@
                 <div class="rentimg">
                   <img src="assets/imgs/rent-3.png" alt="">
                 </div>
-                <p class="title">URSA mini 4.6k Pro + Ac charger</p>
+                <p class="title">Regus Office - Reykjavik</p>
                 <p class="location">Reykjavik, Iceland</p>
               </div>
 
@@ -98,27 +141,70 @@
                 <div class="rentimg">
                   <img src="assets/imgs/rent-4.png" alt="">
                 </div>
-                <p class="title">URSA mini 4.6k Pro + Ac charger</p>
+                <p class="title">MPC midi controller</p>
                 <p class="location">Reykjavik, Iceland</p>
               </div>
               
               <div class="rentitem">
                 <div class="rentimg">
+                  <div class="renthrs">
+                    <span>9 hrs</span>
+                  </div>
                   <img src="assets/imgs/rent-5.png" alt="" style="transform: translate(40%,-40%);">
                 </div>
-                <p class="title">URSA mini 4.6k Pro + Ac charger</p>
+                <p class="title">Skypanel SC60</p>
                 <p class="location">Reykjavik, Iceland</p>
               </div>
 
               <div class="rentitem">
                 <div class="rentimg">
+                  <div class="renthrs">
+                    <span>9 hrs</span>
+                  </div>
                   <img src="assets/imgs/rent-6.png" alt="">
                 </div>
-                <p class="title">URSA mini 4.6k Pro + Ac charger</p>
+                <p class="title">Sennheiser Lavalier Mic</p>
                 <p class="location">Reykjavik, Iceland</p>
               </div>
 
             </div> 
+          </div>
+        </div>
+        <div class="row browseca">
+          <div class="col-md-12">
+          <h2>Browse all product categories</h2>
+          </div>
+          <div class="col-md-3">
+            <ul>
+              <li><a href="">Cameras</a></li>
+              <li><a href="">Camera accessories</a></li>
+              <li><a href="">Drones</a></li>
+              <li><a href="">Audio recording</a></li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul>
+              <li><a href="">Audio speakers</a></li>
+              <li><a href="">Displays</a></li>
+              <li><a href="">Stabilization</a></li>
+              <li><a href="">Studio space</a></li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul>
+              <li><a href="">Synths</a></li>
+              <li><a href="">Midi Controllers</a></li>
+              <li><a href="">Lenses</a></li>
+              <li><a href="">Computers</a></li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <ul>
+              <li><a href="">Film lighting</a></li>
+              <li><a href="">Photo lighting</a></li>
+              <li><a href="">Office space</a></li>
+              <li><a href="">more...</a></li>
+            </ul>
           </div>
         </div>
       </div>
