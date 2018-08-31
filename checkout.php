5d1f75da1346c007d88b973ff39a5588d7dbdb0a
itsKros <kroshan197@gmail.com>
2018-08-31 19:27:40 +0530

    Cart page partially

---
+++ checkout.php
@@ -0,0 +1,18 @@
+<?php include('header.php'); ?>
+
+<!-- Hero Section -->
+<section id="pageTitleSec" class="inGrid checkout">
+  <div class="container">
+    <div class="row">
+        <div class="col-md-8 content">
+            <h1>Checkout</h1>
+        </div>
+    </div>
+  </div>
+</section>
+
+
+<section id="checkoutSec">
+        
+</section>
+<?php include('footer.php'); ?>
---
+++ myaccount.php
@@ -0,0 +1,18 @@
+<?php include('header.php'); ?>
+
+<!-- Hero Section -->
+<section id="pageTitleSec" class="inGrid myacc">
+  <div class="container">
+    <div class="row">
+        <div class="col-md-8 content">
+            <h1>Checkout</h1>
+        </div>
+    </div>
+  </div>
+</section>
+
+
+<section id="myAccount">
+        
+</section>
+<?php include('footer.php'); ?>
