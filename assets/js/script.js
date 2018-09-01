88f74dae73e599e875d0f5eb1aacc4cbd7e495a5
itsKros <kroshan197@gmail.com>
2018-09-01 13:17:56 +0530

    Misc Changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,10 +107,10 @@ $('a.remove').click(function(){
     $( this ).parent().parent().parent().hide( 400 );
 })
   
-  // Just for testing, show all items
-    $('a.btn.continue').click(function(){
-      $('li.items').show(400);
-    })
+// Just for testing, show all items
+$('a.btn.continue').click(function(){
+    $('li.items').show(400);
+})
   
     
 
